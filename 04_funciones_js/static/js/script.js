85b12be85404d2a5fe4792f7f9a491f4c3656ec3
Jheimy Tolentino <jheimytolentino@liceovvh.cl>
2026-03-25 15:10:11 -0300

    hecho

--- 04_funciones_js/static/js/script.js
+++ 04_funciones_js/static/js/script.js
@@ -62,8 +62,7 @@ function ingresarUrgencia() {
 }
 
 /*Ejercicio 3: Sistema de Delivery (shift, pop e if)
-Contexto: Un restaurante tiene pedidos listos. El cajero puede despachar el pedido más antiguo,
- o cancelar el último pedido que entró por un error.
+Contexto: Un restaurante tiene pedidos listos. El cajero puede despachar el pedido más antiguo, o cancelar el último pedido que entró por un error.
 Crea un arreglo: let entregas = ["Pizza", "Sushi", "Hamburguesa", "Ensalada"];
 Función Principal: Crea gestionarPedidos().
 Captura el texto del input. El usuario debe escribir la palabra "despachar" o "cancelar".
@@ -72,4 +71,171 @@ Usa un else if. Si escribió "cancelar", usa .pop() para eliminar la última com
 Muestra en el textContent: "Pedidos pendientes: " seguido del arreglo.
 Limpia el input.
  */
+let entregas = ["Pizza", "Sushi", "Hamburguesa", "Ensalada"];
 
+function actualizarPedidos(pedido) {
+    if (pedido == "despachar") {
+        entregas.shift()
+    } else if (pedido == "cancelar") {
+        entregas.pop()
+    } else {
+        alert("Ingresar un valor valido")
+    }
+    return `Pedidos pendientes: ${entregas.join(", ")}`
+}
+
+function gestionarPedidos() {
+    const container = document.getElementById("container3")
+    const result = document.getElementById("result3")
+    const texto = document.getElementById("input3");
+    let input = texto.value.toLowerCase();
+    let resultado = actualizarPedidos(input);
+    result.textContent = resultado;
+    texto.value = "";
+    container.classList.remove("d-none");
+}
+
+
+
+/*Ejercicio 4: Validador de Códigos de Descuento (for e if)
+Contexto: Una tienda online revisa si el código promocional que ingresó el cliente existe en su base de datos para aplicarle una rebaja.
+Crea un arreglo: let codigosValidos = ["VERANO2026", "PROMO50", "CLIENTEVIP"];
+Función Principal: Crea verificarCodigo().
+Captura el código desde el input.
+Crea una variable mensaje = "Código inválido o expirado";.
+Recorre el arreglo con un ciclo for.
+Si el elemento actual del ciclo es igual al texto del input, cambia la variable mensaje a "¡Éxito! Código aceptado".
+Fuera del ciclo, inyecta la variable mensaje en el textContent del párrafo.
+Limpia el input.
+ */
+let codigosValidos = ["VERANO2026", "PROMO50", "CLIENTEVIP"];
+function buscarCodigpo(codigo) {
+    let mensaje = "Codigo invalido o expírado";
+    for (let i = 0; i < codigosValidos.length; i++) {
+        if (codigo === codigosValidos[i]) {
+            mensaje = "Exito codigo aceptado";
+            return "Exito codigo aceptado";
+        } else {
+            mensaje = "Ingresar un codigo valido";
+        }
+    }
+    return mensaje
+}
+function verificarCodigo() {
+    let input = document.getElementById("input4");
+    let codigo = input.value;
+    const result = document.getElementById("result4")
+    const container = document.getElementById("container4");
+    let resultado = buscarCodigo(codigo);
+    result.textContent = resultado;
+    input.value = "";
+    container.classList.remove("d-none");
+}
+
+
+/*Ejercicio 5: Simulador de Cuotas (for)
+Contexto: Un cliente compra un producto y el sistema le genera automáticamente las etiquetas para sus próximas 3 letras de pago.
+Función Principal: Crea simularCuotas().
+Captura el nombre del producto desde el input (ej: "Bicicleta").
+Crea una variable vacía: registroPagos = "";
+Crea un ciclo for que dé exactamente 3 vueltas (del 1 al 3).
+En cada vuelta, súmale (+=) a registroPagos el producto y el número de la cuota (Ej: producto + " - Cuota " + i + " | ").
+Fuera del ciclo, muestra la variable registroPagos en el textContent del párrafo.
+Limpia el input.
+ */
+function calcularCuotas(valor, cuota) {
+    let registroPagos = "";
+    for (let i = 1; i <= 3; i++) {
+        registroPagos += `Cuota ${i} de ${cuota}: ${parseInt(valor / 3)} \n`
+    }
+    return registroPagos;
+}
+
+function simularCuotas() {
+    const producto = document.getElementById('input5_1');
+    let valorProducto = parseInt(producto.value);
+    const cuotaInput = document.getElementById('input5_2');
+    let cuota = parseInt(cuotaInput.value);
+    const result = document.getElementById('result5');
+    const container = document.getElementById('resultadoContainer5');
+    let resultado = calcularCuotas(valorProducto, cuota);
+    result.textContent = resultado;
+    producto.value = "";
+    cuotaInput.value = "";
+    container.classList.remove('d-none');
+}
+
+
+/*Ejercicio 6: Filtro de Presupuesto (for e if)
+Contexto: Una vitrina virtual tiene varios precios. El cliente ingresa cuánta
+ plata tiene en el bolsillo, y el sistema le muestra solo los precios que le alcanza para pagar.
+Crea un arreglo de precios: let vitrina = [2500, 15000, 8000, 30000, 5000];
+Función Principal: Crea filtrarPrecios().
+Captura el número desde el input (este será el presupuesto del cliente. Recuerda usar Number()).
+Crea una variable opciones = "Te alcanza para los precios: ";
+Recorre el arreglo vitrina con un for.
+Dentro del ciclo, usa un if. Si el precio actual del arreglo es menor o igual (<=) a la plata que ingresó el cliente, súmalo a la variable opciones más un guion (-).
+Muestra el resultado en el textContent del párrafo.
+Limpia el input.
+ */
+let vitrina = [2500, 15000, 30000, 5000];
+let opciones = [];
+function comprobarPresupuesto(presupuesto) {
+    for (let i = 0; i <= vitrina.length; i++) {
+        if (presupuesto >= vitrina[i]) {
+            opciones.push(vitrina[i]);
+        };
+    };
+    if (opciones == "") {
+        return "No te alcanza para nada."
+    } else {
+        return `Te alcanza para los precios: ${opciones.join(" - ")}`
+    }
+};
+function filtrarPrecios() {
+    const input = document.getElementById('input6');
+    const container = document.getElementById('resultadoContainer6');
+    const result = document.getElementById('result6');
+    let dinero = parseInt(input.value);
+    if (isNaN(dinero)) {
+        alert("Ingresa valores válidos.");
+    } else {
+        let resultado = comprobarPresupuesto(dinero);
+        result.textContent = resultado;
+        input.value = "";
+        container.classList.remove("d-none");
+        opciones = [];
+    }
+}
+
+
+/*Ejercicio 7: Cálculo de Sueldo Líquido (Una función llama a otra)
+Contexto: El usuario ingresa su Sueldo Bruto.
+Una función matemática oculta descuenta el 20% (AFP y Salud) y le devuelve el
+dato a la pantalla para mostrar cuánto dinero real recibirá a fin de mes.
+Función Ayudante (La Matemática): Crea una función calcularDescuentos(bruto). 
+Esta función multiplica el bruto por 0.8 y usa return para devolver el resultado.
+Función Principal (La Interfaz): Crea procesarSueldo(). (Esta va en el botón).
+Captura el sueldo desde el input y conviértelo a número.
+Dentro de procesarSueldo, llama a tu función calcularDescuentos() pasándole el número capturado,
+ y guarda lo que te devuelve en una variable llamada sueldoLiquido.
+Modifica el textContent del párrafo: "Tu sueldo a pago es: $" + sueldoLiquido.
+Limpia el input.
+ */
+
+
+
+/*Ejercicio 8: El Carrito de Compras (Reto Final - Delegación de tareas)
+Contexto: Vamos agregando productos al carrito. 
+Una función anota el producto y le pide ayuda a otra función para que dibuje el carrito actualizado en la pantalla.
+Crea un arreglo vacío: let carrito = [];
+Función Ayudante (La Pantalla): Crea actualizarPantalla(). 
+Esta función tomará el arreglo carrito, lo unirá en un texto (ej: carrito.join(" - ")) y 
+lo inyectará en el textContent del párrafo.
+Función Principal (La Lógica): Crea agregarAlCarrito(). (Esta va en el botón).
+Captura el producto desde el input.
+Si el input no está vacío, usa .push() para agregarlo al carrito.
+¡Limpia el input!
+Al final de esta función, llama a actualizarPantalla() para que la vista se refresque 
+automáticamente con el nuevo producto.
+ */
